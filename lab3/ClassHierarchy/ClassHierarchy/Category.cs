b983a1f0d42a297a39efd6a011e59819f2c71ece
julElliot <kozlova_yulia2@mail.ru>
2019-04-21 22:04:01 +0300

    Add 3-d lab

---
+++ lab3/ClassHierarchy/ClassHierarchy/Category.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHierarchy
+{
+    [Serializable]
+    public class Category
+    {
+        public string NatureOfInfo
+        {
+            get; set;
+        }
+        public string TargetAudience
+        {
+            get; set;
+        }
+        public bool Originality
+        {
+            get; set;
+        }
+
+        public Category() {}
+
+        public Category(string natureOfInfo, string targetAudience, bool originality)
+        {
+            NatureOfInfo = natureOfInfo;
+            TargetAudience = targetAudience;
+            Originality = originality;
+        }
+    }
+}
---
+++ lab3/ClassHierarchy/ClassHierarchy/Mini_Issue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHierarchy
+{
+    [Serializable]
+    public class Mini_Issue: Issue
+    {
+        public string Kind
+        {
+            get; set;
+        }
+
+        public Mini_Issue() {}
+
+        public Mini_Issue(Category category, string name, int edition, bool periodicity, string kind)
+            : base(category, name, edition, periodicity)
+        {
+            Kind = kind;
+        }
+    }
+}
